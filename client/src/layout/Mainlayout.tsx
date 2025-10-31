1cfd4bd590fca20c9d9708ba282c5acf2be438b9
Community-Programmer <sarthakpatel230204@gmail.com>
2025-10-31 13:22:09 +0530

    feat: integrate Redux for state management, add ProtectedRoute component, and configure backend URL

--- client/src/layout/Mainlayout.tsx
+++ client/src/layout/Mainlayout.tsx
@@ -2,7 +2,6 @@ import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 
-
 const MainLayout = () => {
     return (
         <>
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "@/pages/Home/Home";
 import RootWrapper from "@/layout/RootWrapper";
 import MainLayout from "@/layout/Mainlayout";
-
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 
 const mainLayoutRoutes = [
@@ -13,11 +13,17 @@ const mainLayoutRoutes = [
     }
 ];
 
+
 const router = createBrowserRouter([
     {
         path: "/",
         element: <RootWrapper />,
         children: [
+            {
+                path: "/",
+                element: <ProtectedRoute />,
+                children: [],
+            },
             {
                 path: "/",
                 element: <MainLayout />,
