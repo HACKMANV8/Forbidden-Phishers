2f42558503ff42c5063f2d635bbefecb4377927d
Community-Programmer <sarthakpatel230204@gmail.com>
2025-11-01 03:24:18 +0530

    fix: update MainLayout structure for improved styling and layout consistency

--- client/src/layout/Mainlayout.tsx
+++ client/src/layout/Mainlayout.tsx
@@ -1,12 +1,13 @@
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import '@livekit/components-styles';
+import '@livekit/components-styles/index.css';
 
 const MainLayout = () => {
-
     return (
         <>
-            <div>
+            <div className="flex flex-col min-h-screen bg-gradient-to-b from-gray-50 to-gray-100" data-lk-theme="light">
                 <Navbar />
                 <Outlet />
                 <Footer />
